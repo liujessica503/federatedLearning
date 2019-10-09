82e7e1610c1cc1ce2cc0ff9d5bd13b1bfef5764e
liujessica503 <13080102+liujessica503@users.noreply.github.com>
2019-10-08 23:07:27 -0400

    fixing bugs, take in parameters from BaseModel.py

--- GlobalModel.py
+++ GlobalModel.py
@@ -1,24 +1,24 @@
 from BaseModel import BaseModel
 from typing import Dict, List, Any
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+from keras.optimizers import SGD, RMSprop, Adam
 
 class GlobalModel(BaseModel):
 
 	def __init__(self, parameter_config: dict()):
-		super.__init__(parameter_config)
+		super().__init__(parameter_config)
 
 		self.model = Sequential()
 
-		model.add(
-			Dense(hidden_units=self.layers[0]), 
-			input_dim=self.input_dim, 
-			activation=self.activation
-		)
+		self.model.add(Dense(self.layers[0], input_dim=self.input_dim, activation=self.activation))
 
 		for i in range(1,len(self.layers)):
-			model.add(Dense(hidden_units=self.layers[i]), activation=self.activation)
+			self.model.add(Dense(self.layers[i], activation = self.activation))
 
-		model.add(Dense(1, activation='sigmoid'))
-		model.compile(
+		self.model.add(Dense(1, activation='sigmoid'))
+		self.model.compile(
 			loss=self.loss,
 			optimizer = optimizers.Adam(
 			  	lr=self.lr, 
@@ -31,14 +31,14 @@ class GlobalModel(BaseModel):
 			metrics=['accuracy'],
 		)
 
-	def train(self, X_dict: Any, y_dict: Any)->None:
+	def train(self, X_dict: Any, Y_dict: Any)->None:
 		# To make X, concat each dataFrame in X_dict
 
-		model.fit(X, Y ,epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbose)
+		self.model.fit(X_dict, Y_dict,epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbose)
 
 	def predict(self, X_dict: Any)->List[int]:
 		# To make X, concat all dataFrames in X_dict
-		return model.predict(X).ravel()
+		return self.model.predict(X_dict).ravel()
 
 	def reset(self)->None:
 		pass
