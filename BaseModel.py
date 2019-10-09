ee6a30bbfeb0c86911d071f0f0e9e042610f48fb
liujessica503 <13080102+liujessica503@users.noreply.github.com>
2019-10-08 23:10:36 -0400

    add parameters to save

--- BaseModel.py
+++ BaseModel.py
@@ -9,16 +9,17 @@ class BaseModel(ABC):
 		self.input_dim = parameter_config["input_dim"]
 		self.activation = parameter_config["activation"]
 		self.loss = parameter_config["loss"]
-		self.lr = parameter_config["lr"]
+		self.lr = parameter_config["learn_rate"]
 		self.epochs = parameter_config["epochs"]
 		self.batch_size = parameter_config["batch_size"]
+		self.verbose = parameter_config["verbose"]
 
 	@abstractmethod
 	def train(self, X: Any, y: Any) -> None:
 		raise NotImplementedError
 
 	@abstractmethod
-	def predict(self, X: Any)->List[int]:
+	def predict(self, X: Any)-> List[int]:
 		raise NotImplementedError
 
 	@abstractmethod
