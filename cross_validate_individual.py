5648170da036f6aa9400dc6ae794f6d0ca8ccbdd
Jessica Liu <jessica@Jessicas-MacBook-Pro-2.local>
2019-10-30 21:03:44 -0400

    rewritten to correctly use IndividualModel class

--- cross_validate_individual.py
+++ cross_validate_individual.py
@@ -19,69 +19,169 @@ Maintenance:
 
 '''
 
+# cut the row indices into partial_train/ validate
+# user_day_data_fold = UserDayData(user_day_data.X.iloc[the_rows_we_want], )
+
 import json
 import sys
 import pandas as pd
 import numpy as np
 import glob
 import os
+import copy
+import csv
+import pickle
+# standardize the data
+from sklearn.preprocessing import StandardScaler
 # import user-defined functions
 from IndividualModel import IndividualModel
-from split_train_test_individual import split_train_test_individual
-
+from split_train_test_global import split_train_test_global
+from UserDayData import UserDayData
 
 k = 3
 # split our 142 days of training data into k partitions
 num_val_samples = (71*2) // k
 
-# dictionary, key is user and value is val_loss_list
+# dictionary of dictionaries. Key is user and value is user_loss_by_lr
 user_loss = {}
+
 
 # load user-inputted parameters
 with open(sys.argv[1]) as file:
         parameter_dict = json.load(file)
 
 epochs = parameter_dict['epochs']
+batch_size = parameter_dict['batch_size']
+verbose = parameter_dict['verbose']
+# write cv results as we process each one
+output_path = parameter_dict['output_path']
+# output python dictionary so that we can read it in easily
+cv_dict_file = parameter_dict['cv_dict_file']
 
-# code derived from Chollet 'Deep Learning with Python'
+train_covariates, train_labels, train_user_day_pairs, test_covariates, test_labels, test_user_day_pairs = split_train_test_global(
+    directory = parameter_dict['input_directory'], 
+    cv = parameter_dict['cv'])
 
-for file in os.listdir(parameter_dict['input_directory']):
-    filename = os.fsdecode(file)
-    user = filename[5:11]
-    if filename.lower().endswith(".csv"):
-        full_filepath = os.path.join(parameter_dict['input_directory'], filename)
+train_data = UserDayData(train_covariates, train_labels, train_user_day_pairs)
+test_data = UserDayData(test_covariates, test_labels, test_user_day_pairs)
 
+# this is also used in the IndividualModel train method
+user_list = np.unique([x[0] for x in train_data.user_day_pairs])
 
+for user in user_list:
+
+    # dictionary, key is learning rate and value is val_loss_list
+    user_loss_by_lr = {}
+
+    for curr_lr in (10**-5, 10**-1):
+        # instantiate model with parameters from json file 
+        # AND custom learn rate
+        model = IndividualModel(parameter_config = parameter_dict, custom_lr = curr_lr)
+        # similar to IndividualModel train method
+        user_model = copy.deepcopy(model.template_model)
         # k-fold cross-validation on an individual's data
 
         # list of length k of loss on validation set for this individual
         val_loss_list = []
         for i in range(k):
-            print('processing fold #', i)
+            print('processing fold #', i, ' for user ', user)
 
-            # get all of our training data that we will then partition into k-folds
-            train_covariates, train_labels, test_covariates, test_labels = split_train_test_individual(
-                    file = full_filepath, 
-                    cv = parameter_dict['cv'])
-            # go to next file if we have no test data
-            if train_covariates is not None:
-                val_covariates = train_covariates[i * num_val_samples: (i + 1) * num_val_samples]
-                val_labels = train_labels[i * num_val_samples: (i + 1) * num_val_samples]
-                partial_train_covariates = np.concatenate( [train_covariates[:i * num_val_samples],
-                    train_covariates[(i + 1) * num_val_samples:]], axis=0)
-                partial_train_labels = np.concatenate( [train_labels[:i * num_val_samples],
-                    train_labels[(i + 1) * num_val_samples:]], axis=0)
-                # build the keras model (already compiled)
-                # model = build_model()
-                # compile model
-                model = IndividualModel(parameter_config = parameter_dict)
-                modelFit = model.train(partial_train_covariates, partial_train_labels, validation_data = (val_covariates, val_labels))
-                # get the loss for the last epoch
-                val_loss = modelFit.history['val_loss'][epochs-1]
-                val_loss_list.append(val_loss)
+            # get the indices of our training data
+            # and subset it into indices for validation and training
+            train_indices = train_data._get_rows_for_users([user])
+            val_indices = train_indices[i * num_val_samples:(i + 1) * num_val_samples]
+            partial_train_indices = train_indices[:i * num_val_samples]+ train_indices[(i + 1)*num_val_samples:]
+
+            # extract data corresponding to our indices
+            val_data = UserDayData(train_data.X.iloc[val_indices], train_data.y[val_indices], [train_data.user_day_pairs[i] for idx in val_indices])
+            partial_train_data = UserDayData(train_data.X.iloc[partial_train_indices], train_data.y[partial_train_indices], [train_data.user_day_pairs[i] for idx in partial_train_indices])
+
+
+            user_scaler = StandardScaler().fit(partial_train_data.X)
+            partial_train_data.X = user_scaler.transform(partial_train_data.X)
+            modelFit = user_model.fit(partial_train_data.X, partial_train_data.y, epochs=epochs, batch_size=batch_size, verbose=verbose, validation_data = (val_data.X, val_data.y))
+            # get the loss for the last epoch
+            val_loss = modelFit.history['val_loss'][epochs-1]
+            val_loss_list.append(val_loss)
+
+        # each time we finish processing k-folds, write the validation results
+        with open(output_path, 'a') as write_to_file:
+            file_writer = csv.writer(write_to_file, delimiter=',')
+            file_writer.writerow([user, curr_lr, val_loss_list])
+
         # add user and list of validation losses to dictionary
-        user_loss[user] = val_loss_list
+        user_loss_by_lr[curr_lr] = val_loss_list
 
+    user_loss[user] = user_loss_by_lr
+
+    # save the user_loss dictionary
+    with open(cv_dict_file, 'wb') as dict_file:
+        pickle.dump(user_loss, dict_file)
+
+'''
+# code derived from Chollet 'Deep Learning with Python'
+
+## we'll take user day and then for each user use get_data_for_users
+
+    # k-fold cross-validation on an individual's data
+
+    # list of length k of loss on validation set for this individual
+    val_loss_list = []
+    for i in range(k):
+        print('processing fold #', i)
+
+        # get all of our training data that we will then partition into k-folds
+        train_covariates, train_labels, test_covariates, test_labels = split_train_test_individual(
+                file = full_filepath, 
+                cv = parameter_dict['cv'])
+        # go to next file if we have no test data
+        if train_covariates is not None:
+            val_covariates = train_covariates[i * num_val_samples: (i + 1) * num_val_samples]
+            val_labels = train_labels[i * num_val_samples: (i + 1) * num_val_samples]
+            partial_train_covariates = np.concatenate( [train_covariates[:i * num_val_samples],
+                train_covariates[(i + 1) * num_val_samples:]], axis=0)
+            partial_train_labels = np.concatenate( [train_labels[:i * num_val_samples],
+                train_labels[(i + 1) * num_val_samples:]], axis=0)
+            # build the keras model (already compiled)
+            # model = build_model()
+            # compile model
+            model = IndividualModel(parameter_config = parameter_dict)
+            modelFit = model.train(partial_train_covariates, partial_train_labels, validation_data = (val_covariates, val_labels))
+            # get the loss for the last epoch
+            val_loss = modelFit.history['val_loss'][epochs-1]
+            val_loss_list.append(val_loss)
+    # add user and list of validation losses to dictionary
+    user_loss_by_lr[lr] = val_loss_list
+
+# key: curr_lr, value: dictionary of val_loss by user for each fold
+user_loss_by_lr[lr] = user_loss 
+
+# best_lr_and_val_loss = min(learn_rate_dict.items(), key=operator.itemgetter(1))
+
+
+csv_file = parameter_dict['output_path']
+with open(csv_file, 'wb') as write_to_file:
+    pickle.dump(user_loss_by_lr, write_to_file)
+
+'''
+'''
+
+# to read in dictionary later: 
+with open(csv_file, 'rb') as handle:
+  loss_data = pickle.loads(handle.read())
+
+# get average for each user:
+avgDict = {}
+for k,v in loss_data.items():
+    # of 457 users (didn't iterate over the rest for time)
+    # we have 357 with loss data. 
+    if len(v) != 0:
+    # v is the list of grades for student k
+        avgDict[k] = sum(v)/ float(len(v))
+# using average for each user, get average over all users:
+average_loss = sum( avgDict.values() ) / len(avgDict)
+# 0.919 for lr 0.001 (cross_validation_binary_individual_lr0_001_with_457_users.csv)
+'''
 
 
 '''
