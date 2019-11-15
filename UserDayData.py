b45f9be807a77157eb2530c48639189aadd23d81
Unknown <jackrgoetz@gmail.com>
2019-11-15 17:19:47 -0500

    Adding get methods to UserDayData
    Adding basic getting methods, and making PEP8 happy

--- UserDayData.py
+++ UserDayData.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-
 from typing import Any, List, Tuple
 
 
@@ -11,20 +8,29 @@ class UserDayData:
         self.y = y
         self.user_day_pairs = user_day_pairs
 
+    def get_X(self):
+        return self.X
+
+    def get_y(self):
+        return self.y
+
+    def get_user_day_pairs(self):
+        return self.user_day_pairs
+
     # get by users
 
     def _get_rows_for_users(self, users: List[int])->List[int]:
         rows = []
         for i in users:
             rows = rows + [
-                ind for ind, val in enumerate(self.user_day_pairs) if val[0] == i
+                ind for ind, v in enumerate(self.user_day_pairs) if v[0] == i
             ]
 
         return rows
 
     def get_data_for_users(self, users: List[int])->Any:
         rows = self._get_rows_for_users(users)
-        return self.X.iloc[rows,:], self.y[rows]
+        return self.X.iloc[rows, :], self.y[rows]
 
     # get by days
 
@@ -32,34 +38,33 @@ class UserDayData:
         rows = []
         for i in days:
             rows = rows + [
-                ind for ind, val in enumerate(self.user_day_pairs) if val[1] == i
+                ind for ind, v in enumerate(self.user_day_pairs) if v[1] == i
             ]
         return rows
 
     def get_data_for_days(self, days: List[int])->Any:
         rows = self._get_rows_for_days(days)
-        return self.X.iloc[rows,:], self.y[rows]
+        return self.X.iloc[rows, :], self.y[rows]
 
     # get by users and days
 
     def _get_rows_for_users_on_days(
-            self, users: List[int], days: List[int]
-        )->List[int]:
+        self, users: List[int], days: List[int]
+    )->List[int]:
 
         rows = []
         for i in users:
             for j in days:
                 rows = rows + [
-                    ind for ind, val in enumerate(self.user_day_pairs) if (
-                        val[0] == i and val[1] == j
+                    ind for ind, v in enumerate(self.user_day_pairs) if (
+                        v[0] == i and v[1] == j
                     )
                 ]
         return rows
 
     def get_data_for_users_on_days(
-            self, users: List[int], days: List[int]
-        )->Any:
+        self, users: List[int], days: List[int]
+    )->Any:
 
         rows = self._get_rows_for_users_on_days(users, days)
-        return self.X.iloc[rows,:], self.y[rows]
-
+        return self.X.iloc[rows, :], self.y[rows]
