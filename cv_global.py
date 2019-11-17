f893dc1e44d1ecddf4d09965142030ed805c1f6b
Unknown <jackrgoetz@gmail.com>
2019-11-17 16:24:38 -0500

    Function-izing cv

--- cv_global.py
+++ cv_global.py
@@ -4,27 +4,27 @@ import numpy as np
 
 # import user-defined functions
 from GlobalModel import GlobalModel
+from BaseModel import BaseModel
+from UserDayData import UserDayData
 from collections import defaultdict
 from single_global_experiment import (
     run_single_experiment,
     write_to_json,
     simple_train_test_split,
 )
+from typing import List, Dict
 
 
-def main():
+def run_cv(
+    model_class: BaseModel,
+    train_data: UserDayData,
+    k: int,
+    lrs: List[float],
+    parameter_dict: Dict[str, float]
+)->Dict[str, float]:
 
-    with open(sys.argv[1]) as file:
-        parameter_dict = json.load(file)
-
-    k = 3
-    # split our 142 days of training data into k partitions
     num_val_samples = (71 * parameter_dict["cv"]) // k
-
-    lrs = [10**-5, 10**-4, 10**-3, 10**-2, 10**-1]
     aucs_by_lr = defaultdict(list)
-
-    train_data, test_data = simple_train_test_split(parameter_dict)
 
     for i in range(k):
         val_days = list(range(i * num_val_samples, (i + 1) * num_val_samples))
@@ -36,7 +36,7 @@ def main():
         )
         train_fold = train_data.get_subset_for_days(train_days)
         for lr in lrs:
-            model = GlobalModel(
+            model = model_class(
                 parameter_config=parameter_dict,
                 parameter_overwrite={"lr": lr}
             )
@@ -47,6 +47,21 @@ def main():
 
     for lr in lrs:
         aucs_by_lr[str(lr) + "_avg"] = np.mean(aucs_by_lr[lr])
+
+    return aucs_by_lr
+
+
+def main():
+
+    with open(sys.argv[1]) as file:
+        parameter_dict = json.load(file)
+
+    k = 3
+    lrs = [10**-5, 10**-4, 10**-3, 10**-2, 10**-1]
+
+    train_data, test_data = simple_train_test_split(parameter_dict)
+
+    aucs_by_lr = run_cv(GlobalModel, train_data, k, lrs, parameter_dict)
 
     write_to_json(aucs_by_lr, parameter_dict["output_path"] + "_cv")
 
