be65fcb34b627a4d9d2a49ab5fac8e8b849d4db5
Unknown <jackrgoetz@gmail.com>
2019-11-27 17:49:58 -0500

    Made ExperimentUtils class
    Moved all our functions related to running experiments into the Utils class. This includes getting the correct model from the config json, since we will likely need to use that in many places.

---
+++ ExperimentUtils.py
@@ -0,0 +1,86 @@
+import json
+import csv
+
+# import user-defined functions
+from split_train_test_global import split_train_test_global
+from UserDayData import UserDayData
+from typing import Any, Dict
+
+from IndividualModel import IndividualModel
+from GlobalModel import GlobalModel
+from FedModel import FedModel
+from BaseModel import BaseModel
+
+
+class ExperimentUtils:
+
+    model_registry = {
+        "individual_model": IndividualModel,
+        "global_model": GlobalModel,
+        "fed_model": FedModel,
+    }
+
+    @staticmethod
+    def run_single_experiment(
+        model: BaseModel,
+        train_data: UserDayData,
+        test_data: UserDayData,
+        plotAUC=False
+    )->Dict:
+        model.train(train_data)
+        metrics = model.evaluate(test_data, plotAUC=plotAUC)
+        return metrics
+
+    @staticmethod
+    def write_to_csv(results, output_path: str)->None:
+        csv_columns = list(results.keys())
+        with open(output_path + ".csv", 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            writer.writerow(results)
+
+        print('results written to :' + output_path + ".csv")
+
+    @staticmethod
+    def write_to_json(results, output_path: str)->None:
+        with open(output_path + ".json", "w") as f:
+            json.dump(results, f, indent=4)
+        print('results written to :' + output_path + ".json")
+
+    @staticmethod
+    def simple_train_test_split(parameter_dict: Dict[str, Any])->UserDayData:
+        (
+            train_covariates,
+            train_labels,
+            train_user_day_pairs,
+            test_covariates,
+            test_labels,
+            test_user_day_pairs
+        ) = split_train_test_global(
+            directory=parameter_dict['input_directory'],
+            cv=parameter_dict['cv']
+        )
+
+        train_data = UserDayData(
+            X=train_covariates,
+            y=train_labels,
+            user_day_pairs=train_user_day_pairs
+        )
+        test_data = UserDayData(
+            X=test_covariates,
+            y=test_labels,
+            user_day_pairs=test_user_day_pairs
+        )
+
+        return train_data, test_data
+
+    @staticmethod
+    def model_from_config(model_type):
+        try:
+            model_class = ExperimentUtils.model_registry[model_type]
+            return model_class
+        except KeyError:
+            raise KeyError(
+                'model_type in config json must be one of: "individual_model",'
+                '"global_model", "fed_model"'
+            )
--- single_experiment.py
+++ single_experiment.py
@@ -3,16 +3,8 @@ import json
 import sys
 
 # import user-defined functions
-from IndividualModel import IndividualModel
-from GlobalModel import GlobalModel
-from FedModel import FedModel
 
-from run_single_experiment import (
-    run_single_experiment,
-    write_to_csv,
-    write_to_json,
-    simple_train_test_split,
-)
+from ExperimentUtils import ExperimentUtils
 
 
 def main():
@@ -21,38 +13,30 @@ def main():
     with open(sys.argv[1]) as file:
         parameter_dict = json.load(file)
 
-    train_data, test_data = simple_train_test_split(parameter_dict)
+    train_data, test_data = ExperimentUtils.simple_train_test_split(
+        parameter_dict
+    )
 
     # get the name of the model from the user-inputted json file
     # and match it to the corresponding model object
-    model_registry = {
-        "individual_model": IndividualModel,
-        "global_model": GlobalModel,
-        "fed_model": FedModel,
-    }
+    model_class = ExperimentUtils.model_from_config(
+        parameter_dict['model_type']
+    )
+    model = model_class(parameter_config=parameter_dict)
 
-    if model_registry.get(parameter_dict.get('model_type')) is None:
-        raise KeyError(
-            'model_type in .json must be one of: "individual_model",'
-            '"global_model", "fed_model"'
-        )
-    else:
-        model_class = model_registry[parameter_dict['model_type']]
-
-    if model_class == IndividualModel:
-        model = IndividualModel(parameter_config=parameter_dict)
-    elif model_class == FedModel:
-        model = FedModel(parameter_config=parameter_dict)
-    elif model_class == GlobalModel:
-        model = GlobalModel(parameter_config=parameter_dict)
-
-    results = run_single_experiment(
+    results = ExperimentUtils.run_single_experiment(
         model, train_data, test_data, plotAUC=True
     )
     ind_results = model.individual_evaluate(test_data)
-    write_to_json(ind_results, parameter_dict["output_path"] + "_by_user")
-    write_to_csv(results, parameter_dict["output_path"])
-    write_to_json(results, parameter_dict["output_path"])
+    ExperimentUtils.write_to_json(
+        ind_results, parameter_dict["output_path"] + "_by_user"
+    )
+    ExperimentUtils.write_to_csv(
+        results, parameter_dict["output_path"]
+    )
+    ExperimentUtils.write_to_json(
+        results, parameter_dict["output_path"]
+    )
 
     finish = datetime.datetime.now() - start
     print('Time to finish: ' + str(finish.total_seconds()))
