0ca374f7f2f7e49c7e95cc992d8f43e90515669a
Unknown <jackrgoetz@gmail.com>
2019-11-29 19:46:32 -0500

    Further splitting the OutputLayer class

--- OutputLayer.py
+++ OutputLayer.py
@@ -4,32 +4,46 @@ from keras.layers import Dense
 
 class OutputLayer(ABC):
 
+    NAME = None
+
     @classmethod
     def from_config(cls, output_layer_dict):
-        if output_layer_dict["loss_type"] == "classification":
-            return ClassificationLayer(output_layer_dict)
-        elif output_layer_dict["loss_type"] == "regression":
+        if output_layer_dict["loss_type"] == "regression":
             return RegressionLayer(output_layer_dict)
+        elif output_layer_dict["loss_type"] == "classification":
+            if len(
+                output_layer_dict["classification_thresholds"]
+            ) == 1:
+                return BinaryLayer(output_layer_dict)
+            else:
+                return MultiClassLayer(output_layer_dict)
 
 
-class ClassificationLayer(OutputLayer):
+class BinaryLayer(OutputLayer):
+
+    NAME = "BinaryLayer"
+
+    def __init__(self, output_layer_dict):
+        self.layer = Dense(1, activation='sigmoid')
+        self.loss = "binary_crossentropy"
+
+
+class MultiClassLayer(OutputLayer):
+
+    NAME = "MultiClassLayer"
 
     def __init__(self, output_layer_dict):
         self.layer = Dense(
             len(output_layer_dict["classification_thresholds"]),
             activation='sigmoid',
         )
-        if len(
-            output_layer_dict["classification_thresholds"]
-        ) == 1:
-            self.loss = "binary_crossentropy"
-
-        else:
-            self.loss = "categorical_crossentropy"
+        self.loss = "categorical_crossentropy"
 
 
 class RegressionLayer(OutputLayer):
 
+    NAME = "RegressionLayer"
+
     def __init__(self, output_layer_dict):
-        self.layer = Dense(1, "relu")
+        self.layer = Dense(1)
         self.loss = "mean_squared_error"
